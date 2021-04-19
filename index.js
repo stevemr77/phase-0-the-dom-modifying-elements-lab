8410eca8b7d4cbc413ab8946573e94d14173cc7e
Michael <stevemr77@gmail.com>
2021-04-18 18:44:31 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Michael is the champion';
+
